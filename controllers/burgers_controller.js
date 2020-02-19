d9b401f18bae13889a36a01af92d81c730b1ab40
shwetakelkar <shweta4.gupta@gmail.com>
2020-02-18 21:53:25 -0600

    changes made for empty field

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,29 +4,30 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
+var dbName = process.env.CURRENT_DBNAME;
+
 router.get("/Index",function(req,res){
     burger.getAllBurger(function(data){
-        res.render("index",{allBurgers:data})
+        res.render("index",{allBurgers:data,db:dbName})
     });
 
 });
 
 router.post("/api/burgers",function(req,res){
 
-    
     var burgerName = req.body.burger;
-    
-    burger.addBurger(burgerName,function(data){
-           res.redirect("/Index"); 
-    })
-
+    if(burgerName != ""){
+        burger.addBurger(burgerName,function(data){
+            res.redirect("/Index"); 
+        })
+    }
 })
 
 router.put("/api/burgers/:id",function(req,res){
 
     burger.updateBurgerInfo(req.body.devoured,req.params.id,function(data){
 
-        res.render("index",{allBurgers:data})
+        res.render("index",{allBurgers:data,db:dbName})
 
     })
 
